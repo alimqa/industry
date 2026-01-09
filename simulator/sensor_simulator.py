bac0278cdb655c29b18f83730c3bf23697ec6eb4
alimqa <babakpayeva.alima@gmail.com>
2026-01-09 12:08:03 +0500

    Update sensor_simulator.py

--- simulator/sensor_simulator.py
+++ simulator/sensor_simulator.py
@@ -1,4 +1,17 @@
-# IoT Sensor Simulator
-# Sends temperature and humidity data via HTTP
+import requests
+import random
+import time
 
-print("Sensor simulator started")
+URL = "http://localhost:5000/data"
+
+while True:
+    payload = {
+        "device_id": "sensor_01",
+        "temperature": round(random.uniform(20, 30), 2),
+        "humidity": round(random.uniform(40, 60), 2)
+    }
+
+    response = requests.post(URL, json=payload)
+    print(response.json())
+
+    time.sleep(5)
